59753a817076da875f0ddbfd77addbc13266b3b0
Eksa <eksoct@yahoo.com>
2018-03-08 20:46:01 +0700

    Setup project

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+module.exports = app;
