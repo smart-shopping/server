fce72cf7daa7e32dfabe44b128f550e7b2c51819
Eksa <eksoct@yahoo.com>
2018-03-09 14:50:05 +0700

    Update token spotify

--- controllers/music.js
+++ controllers/music.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const request = require('request')
+const Spotify = require('machinepack-spotify');
 
 const paramsRecognition = {
   "returnFaceId": "true",
@@ -9,118 +10,99 @@ const paramsRecognition = {
 
 module.exports = {
   getMusicAll: (req, res) => {
-    axios({
-      method: 'post',
-      url:'https://accounts.spotify.com/api/token',
-      params: {
-        client_id: process.env.SPOTIFY_ID,
-        client_secret: process.env.SPOTIFY_SECRET,
-        code: 'spotify',
-        scope: 'user-read-private user-read-email',
-        grant_type :'authorization_code',
-        redirect_uri: process.env.SPOTIFY_REDIRECT
-      },
+    request({
+      method: 'GET',
+      uri: `https://api.spotify.com/v1/search?q=${strongestEmotion}&type=playlist&market=ID`,
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + process.env.SPOTIFY_TOKEN,
+      },
+    }, (error, response, body) => {
+      if (error) {
+        return console.error('upload failed:', error);
       }
-    })
-    .then(token => {
-      res.status(200).send(token)
-    })
-    .catch(err => {
-      console.error(err)
-      // res.status(400).send(err)
+      console.log('Server responded with:', body);
+      res.send(body)
     })
   },
 
   getMusicByMood: (req, res) => {
-    // console.log('req.body.sourceImage == ', req.body.sourceImage);
-    // console.log('process.env.SUBSCRIP_KEY == ', process.env.SUBSCRIP_KEY);
-    // axios({
-    //   url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,accessories,exposure',
-    //   type: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Ocp-Apim-Subscription-Key': process.env.SUBSCRIP_KEY
-    //   },
-    //   data: {
-    //     url: req.body.sourceImage
-    //   }
-    // })
-    // .then(resp => {
-    //   console.log('resp == ', resp);
-    //   res.status(200).send(JSON.stringify(resp, null, 2))
-    // })
-    // .catch(err => {
-    //   console.error(err)
-    //   res.status(400).send(err)
-    // })
-
-    // axios({
-    //   url: `https://api.spotify.com/v1/search?q=${req.body.emotion}&type=playlist&market=ID`,
-    //   type: 'GET',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Authorization': 'Bearer BQBtICw_1uaVsXqX6nfmdlQ6B6eQN16SQb0UtRXKOh3qLlqEymSgnTeaA3MYkCiHZ5pyOvx1muuCxToBrtOf6QYOAMJzsiK9kQgP_JrADAH6V-zMRdwA7sIRA-gGf_yBW3YzWf4QhqBXkKHBNE_JqLHA-5SzvB_TQA',
-    //     'Content-Type': 'application/x-www-form-urlencoded'
-    //   }
-    // })
-    // axios.get(`https://api.spotify.com/v1/search?q=happiness&type=playlist&market=ID`, JSON.stringify({
-    //   'Accept': 'application/json',
-    //   'Authorization': 'Bearer BQBtICw_1uaVsXqX6nfmdlQ6B6eQN16SQb0UtRXKOh3qLlqEymSgnTeaA3MYkCiHZ5pyOvx1muuCxToBrtOf6QYOAMJzsiK9kQgP_JrADAH6V-zMRdwA7sIRA-gGf_yBW3YzWf4QhqBXkKHBNE_JqLHA-5SzvB_TQA',
-    // }))
-    // .then(resp => {
-    //   res.send(resp.data)
-    // })
-    // .catch(err => res.status(500).send(err))
-
-    request({
-      method: 'POST',
-      uri: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,accessories,exposure',
-      headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': process.env.SUBSCRIP_KEY
+    Spotify.getAccessToken({
+      clientId: process.env.SPOTIFY_ID,
+      clientSecret: process.env.SPOTIFY_SECRET,
+    })
+    .exec({
+      error: function (err) {
+        console.log(err);
       },
-      json: {
-        'url': req.body.sourceImage
-      }
-    }, (error, response, body) => {
-      if (error) {
-        return console.error('Server failed:', error);
-      }
-      // console.log('Server responded with:', body[0].faceAttributes.emotion);
-      let arrEmotion = []
-      let strongestEmotion = '';
-      let emotionScore = 0;
-      let emotion = body[0].faceAttributes.emotion
-      delete emotion.contempt
-      delete emotion.disgust
-      delete emotion.surprise
-      delete emotion.neutral
-      arrEmotion.push(emotion)
+      success: function (token) {
+        console.log('OKEEE ', token);
+        request({
+          method: 'POST',
+          uri: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,accessories,exposure',
+          headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': process.env.SUBSCRIP_KEY
+          },
+          json: {
+            'url': req.body.sourceImage
+          }
+        }, (error, response, body) => {
+          if (error) {
+            return console.error('Server failed:', error);
+          }
+          // console.log('Server responded with:', body[0].faceAttributes.emotion);
+          let arrEmotion = []
+          let strongestEmotion = '';
+          let emotionScore = 0;
+          let emotion = body[0].faceAttributes.emotion
+          delete emotion.contempt
+          delete emotion.disgust
+          delete emotion.surprise
+          delete emotion.neutral
+          arrEmotion.push(emotion)
 
-      for (var key in emotion) {
-        if (emotion[key] > emotionScore) {
-          emotionScore = emotion[key];
-          strongestEmotion = key;
-        }
-      }
-      console.log(strongestEmotion);
+          for (var key in emotion) {
+            if (emotion[key] > emotionScore) {
+              emotionScore = emotion[key];
+              strongestEmotion = key;
+            }
+          }
+          console.log(strongestEmotion);
 
-      request({
-        method: 'GET',
-        uri: `https://api.spotify.com/v1/search?q=${strongestEmotion}&type=playlist&market=ID`,
-        headers: {
-          'content-type': 'application/json',
-          'Authorization': 'Bearer ' + process.env.SPOTIFY_TOKEN,
-        },
-      }, (error, response, body) => {
-        if (error) {
-          return console.error('upload failed:', error);
-        }
-        console.log('Server responded with:', body);
-        res.send(body)
-      })
+          request({
+            method: 'GET',
+            uri: `https://api.spotify.com/v1/search?q=${strongestEmotion}&type=playlist&market=ID`,
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': 'Bearer ' + token, // + process.env.SPOTIFY_TOKEN,
+            },
+          }, (error, response, body) => {
+            if (error) {
+              return console.error('upload failed:', error);
+            }
+            // console.log('Server responded with:', body);
+            res.send(body)
+          })
+        })
+
+        Spotify.getLoginUrl({
+          clientId: 'abc123jdhs3h4js',
+          redirectUri: 'https://example.com/callback?code=NApCCg..BkWtQ&state=profile%2Factivity',
+          responseType: [ 'code' ],
+          state: '34fFs29kd09',
+          scope: '*',
+          dialog: 'false',
+        })
+        .exec({
+          error: function (err) {
+            console.log(err);
+          },
+          success: function (result) {
+            // console.log(result);
+          },
+        });
+      },
     })
   }
 }
