aee4ce4d1571d438110a2ca54547f9238af3eca3
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-03-09 10:00:33 +0700

    release login fb

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cors = require('cors');
 
 var app = express();
 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/moodrecognition')
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+    check : (req,res,next) => {
+        let providedToken   = req.headers.token;;
+        if (providedToken) {
+            try {
+                let decoded     = jwt.verify(providedToken, process.env.SECRET)
+                if(decoded) {
+                    next()
+                }
+
+            } catch(err) {                
+                res.status(401).json({
+                    message : 'Wrong token provided, authentication failed !'
+                })
+            }
+        } else {
+            res.status(401).json({
+                message : 'Please provide a token !'
+            })
+        }
+    }
+}
